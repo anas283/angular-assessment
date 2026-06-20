e1e83261083d714754bdf8d9ebb4cf7a0e62d51c
Muhammad Anas <muhammadanasbinadam@gmail.com>
2026-06-20 22:49:18 +0800

    chore: polish readme

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aem-dashboard'`, () => {
+  it(`should have as title 'angular-assessment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aem-dashboard');
+    expect(app.title).toEqual('angular-assessment');
   });
 
   it('should render router outlet', () => {
